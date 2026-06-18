319296e7e0d10ac89b4a797d995cf69b1c6dfe0c
Darshan N <darshannahata5555@gmail.com>
2026-06-19 02:32:52 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,17 +21,24 @@ public class Main {
                 }
                 else if(command.startsWith("type"))
                 {
-                    if(command.equals("type echo"))
+                    // if(command.equals("type echo"))
+                    // {
+                    //     System.out.println("echo is a shell builtin");
+                    // }
+                    // else if(command.equals("type exit"))
+                    // {
+                    //     System.out.println("exit is a shell builtin");
+                    // }
+                    // else if(command.equals("type type"))
+                    // {
+                    //     System.out.println("type is a shell builtin");
+                    // }
+                    if(command.startsWith("type"))
                     {
-                        System.out.println("echo is a shell builtin");
-                    }
-                    else if(command.equals("type exit"))
-                    {
-                        System.out.println("exit is a shell builtin");
-                    }
-                    else if(command.equals("type type"))
-                    {
-                        System.out.println("type is a shell builtin");
+                        if(command.equals("echo") || command.equals("exit") || command.equals("type"))
+                        {
+                            System.out.print(command.substring(5) + " is a shell builtin");
+                        }
                     }
                     else
                     {
