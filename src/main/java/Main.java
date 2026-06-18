a78e1feebc0d03b493d43aa1c0b367703bbd4903
Darshan N <darshannahata5555@gmail.com>
2026-06-19 02:26:15 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,17 +11,37 @@ public class Main {
                 String command = scanner.nextLine();
                 
                 if(command.equals("exit"))
-                    {
-                        break;
-                    }
+                {
+                    break;
+                }
                 else if(command.startsWith("echo"))
+                {
+                    System.out.println(command.substring(5));
+                    
+                }
+                else if(command.startsWith("type"))
+                {
+                    if(command.equals("echo"))
                     {
-                        System.out.println(command.substring(5));
+                        System.out.println("echo is a shell builtin");
                     }
+                    else if(command.equals("exit"))
+                    {
+                        System.out.println("exit is a shell builtin");
+                    }
+                    else if(command.equals("type"))
+                    {
+                        System.out.println("type is a shell builtin");
+                    }
+                    else
+                    {
+                        System.out.println(command+ ": not found");
+                    }
+                }
                 else 
-                    {
-                        System.out.println(command + ": command not found");
-                    }
+                {
+                    System.out.println(command + ": command not found");
+                }
             }
     }
 }
