4642a54c923f99a3f9397fe3de0d36062aaee70f
Darshan N <darshannahata5555@gmail.com>
2026-06-19 02:04:30 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
-        while(command != "exit")
+        while(true)
             {
                 System.out.print("$ ");
 
@@ -11,6 +11,10 @@ public class Main {
                 String command = scanner.nextLine();
 
                 System.out.println(command + ": command not found");
+                if(command.equals("exit"))
+                {
+                    break;
+                }
             }
     }
 }
